61e2071619d3affc04c520950a44727f5f2840fc
Zillomoons <dlzht73@ya.ru>
2022-05-06 09:46:24 +0300

    use nodemailer in post method

--- sendMessage/contact.js
+++ sendMessage/contact.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+
+const smtp_login = process.env.SMTP_LOGIN
+const smtp_password = process.env.SMTP_PASSWORD
+const end_mail = process.env.END_MAIL
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: smtp_login,
+        pass: smtp_password
+    }
+})
 
 router.get('/', async (req, res) => {
     try {
@@ -11,7 +24,19 @@ router.get('/', async (req, res) => {
         return res.status(500).send('Server error')
     }
 })
+
 router.post('/', (req, res) => {
+    const { name, contacts, subject, message } = req.body.data;
+    transporter.sendMail({
+        from: `Portfolio`,
+        to: end_mail,
+        subject: 'Portfolio-message',
+        html: `<b>${subject}</b>
+        <em>From: ${name}</em>
+        <div>Contacts: ${contacts}</div>
+        <div>${message}</div>
+        `
+    });
     res.send('Hell yeah! Letter is send')    
 })
 
