2dc25dd4bc718777b58117c88c7d7d9a7bc6d4ee
Zillomoons <dlzht73@ya.ru>
2022-05-06 09:25:58 +0300

    add cors, bodyParser, nodemailer packages

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const contact = require('./sendMessage/contact')
+const contact = require('./sendMessage/contact');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 app.use('/sendMessage', contact);
+
+
 /* app.get('/', (req, res) => {
   res.send('Hello NodeJS!')
 }) */
